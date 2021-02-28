b8659af64a9817162367158155fca6402b4b5db0
Art_Chen <Chenxy0201@qq.com>
2021-02-28 13:52:07 +0800

    audio: update heart audio config

--- device.mk
+++ device.mk
@@ -9,3 +9,10 @@ $(call inherit-product-if-exists, vendor/lenovo/heart/heart-vendor.mk)
 
 # sm8150-common
 $(call inherit-product, device/lenovo/sm8150-common/common.mk)
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml
